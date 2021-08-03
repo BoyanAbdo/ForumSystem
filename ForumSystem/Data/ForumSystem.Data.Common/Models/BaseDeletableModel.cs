7dbaeba3ceb041484823c20969aba3e38cd906eb
Boyan <boyan.abdo@gmail.com>
2021-08-03 16:30:26 +0300

    Initial Commit

---
+++ ForumSystem/Data/ForumSystem.Data.Common/Models/BaseDeletableModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumSystem.Data.Common.Models
+{
+    using System;
+
+    public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>, IDeletableEntity
+    {
+        public bool IsDeleted { get; set; }
+
+        // Nullable
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ ForumSystem/Data/ForumSystem.Data/IdentityOptionsProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace ForumSystem.Data
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public static class IdentityOptionsProvider
+    {
+        public static void GetIdentityOptions(IdentityOptions options)
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 5;
+            options.Lockout.AllowedForNewUsers = true;
+            options.Lockout.MaxFailedAccessAttempts = 3;
+        }
+    }
+}
